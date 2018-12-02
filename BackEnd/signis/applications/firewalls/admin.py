197f834c932bbb92f9c1db4c5955525f8a936c8a
Tu Nombre <you@example.com>
2018-12-02 19:31:29 +0100

    BackEnd Added

---
+++ BackEnd/signis/applications/firewalls/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from applications.firewalls.models import firewalls
+
+# Register your models here.
+admin.site.register(firewalls)

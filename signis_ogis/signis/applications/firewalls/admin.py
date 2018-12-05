f1a80080c2795cd21a36dc32cc96e5b1e8dc254e
Tu Nombre <you@example.com>
2018-12-05 10:33:23 +0100

    Deleted

--- signis_ogis/signis/applications/firewalls/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from applications.firewalls.models import firewalls
-
-# Register your models here.
-admin.site.register(firewalls)
--- signis_ogis/signis/applications/users/urls.py
+++
@@ -1,7 +0,0 @@
-"""Users URL Configuration"""
-from django.conf.urls import url
-from applications.users.views import UserRegister
-
-urlpatterns = [
-    url(r'register', UserRegister.as_view(), name='registration')
-]

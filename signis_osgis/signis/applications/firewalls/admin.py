cf322c0c0e9f42e73aeecf8a59a0603a2f2bcbe7
Tu Nombre <you@example.com>
2018-12-06 19:29:34 +0100

    Project Updated

--- signis_osgis/signis/applications/firewalls/admin.py
+++ signis_osgis/signis/applications/firewalls/admin.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from applications.firewalls.models import firewalls
+from django.contrib.gis import admin
 
 # Register your models here.
-admin.site.register(firewalls)
+admin.site.register(firewalls, admin.OSMGeoAdmin)
--- signis_osgis/signis/applications/firewalls/models.py
+++ signis_osgis/signis/applications/firewalls/models.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.gis.db import models
+from django.contrib.gis.geos import GEOSGeometry
 
 # Create your models here.
 
 class firewalls(models.Model):
-    location = models.LineStringField(srid=3857)
+    geom = models.MultiLineStringField(srid=3857)
     type = models.IntegerField()
     descript = models.CharField(max_length=70)

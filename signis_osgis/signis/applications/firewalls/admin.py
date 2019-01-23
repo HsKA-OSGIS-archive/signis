6e306632c53849dd19404eadd76b3b051ed2571a
jesusn95 <saje1016@hs-karlsruhe.de>
2019-01-23 15:14:50 +0300

    index updated

--- signis_osgis/signis/applications/firewalls/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from applications.firewalls.models import firewalls
-from django.contrib.gis import admin
-
-# Register your models here.
-admin.site.register(firewalls, admin.OSMGeoAdmin)

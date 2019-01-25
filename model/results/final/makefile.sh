272b500277be59a84cf18366c76ff3cf73792d1b
CarlosBayarri <bayarricebrecos@hotmail.com>
2019-01-25 15:29:35 +0100

    makefile for geojson

--- model/results/final/makefile.sh
+++ model/results/final/makefile.sh
@@ -1,14 +1,14 @@
-gdal_translate ../reclass/siose.tif default_model_january.tif
-gdal_translate ../reclass/siose.tif default_model_february.tif
-gdal_translate ../reclass/siose.tif default_model_march.tif
-gdal_translate ../reclass/siose.tif default_model_april.tif
-gdal_translate ../reclass/siose.tif default_model_may.tif
-gdal_translate ../reclass/siose.tif default_model_juny.tif
-gdal_translate ../reclass/siose.tif default_model_july.tif
-gdal_translate ../reclass/siose.tif default_model_august.tif
-gdal_translate ../reclass/siose.tif default_model_september.tif
-gdal_translate ../reclass/siose.tif default_model_october.tif
-gdal_translate ../reclass/siose.tif default_model_november.tif
-gdal_translate ../reclass/siose.tif default_model_december.tif
-gdal_translate ../reclass/siose.tif own_model.tif
+gdal_translate ./risk1.tif risk1.tif
+gdal_translate ./risk2.tif risk2.tif
+gdal_translate ./risk3.tif risk3.tif
+gdal_translate ./risk4.tif risk4.tif
+gdal_translate ./risk5.tif risk5.tif
+gdal_translate ./risk6.tif risk6.tif
+gdal_translate ./risk7.tif risk7.tif
+gdal_translate ./risk8.tif risk8.tif
+gdal_translate ./risk9.tif risk9.tif
+gdal_translate ./risk10.tif risk10.tif
+gdal_translate ./risk11.tif risk11.tif
+gdal_translate ./risk12.tif risk12.tif
+gdal_translate ./riskuser.tif riskuser.tif
 

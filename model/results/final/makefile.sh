506eeb4b8158bd826c8ac8607ded99b807167fb7
CarlosBayarri <bayarricebrecos@hotmail.com>
2019-01-25 15:18:17 +0100

    script for geotiff

---
+++ model/results/final/makefile.sh
@@ -0,0 +1,14 @@
+gdal_translate ../reclass/siose.tif default_model_january.tif
+gdal_translate ../reclass/siose.tif default_model_february.tif
+gdal_translate ../reclass/siose.tif default_model_march.tif
+gdal_translate ../reclass/siose.tif default_model_april.tif
+gdal_translate ../reclass/siose.tif default_model_may.tif
+gdal_translate ../reclass/siose.tif default_model_juny.tif
+gdal_translate ../reclass/siose.tif default_model_july.tif
+gdal_translate ../reclass/siose.tif default_model_august.tif
+gdal_translate ../reclass/siose.tif default_model_september.tif
+gdal_translate ../reclass/siose.tif default_model_october.tif
+gdal_translate ../reclass/siose.tif default_model_november.tif
+gdal_translate ../reclass/siose.tif default_model_december.tif
+gdal_translate ../reclass/siose.tif own_model.tif
+
